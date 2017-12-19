03f36c6573bfe2d6e9bdc2a00dbba39dc758a4d2
cheeseconnoisseur <arturo.dezon@gmail.com>
2017-12-19 15:06:23 +0000

    optimazing

--- code.js
+++ code.js
@@ -1,343 +1,81 @@
-
 //code for the header it sets the content and colour of the top banner
-function openColour(cityName, elmnt, color) {
-    // Hide all elements with class="tabcontent" by default */
-    var i, tabcontent, tablinks;
-    tabcontent = document.getElementsByClassName("tabcontent");
-    for (i = 0; i < tabcontent.length; i++) {
-        tabcontent[i].style.display = "none";
-    }
+function openColour(t,e,n){
+// Hide all elements with class="tabcontent" by default */
+var i,o,a
+for(o=document.getElementsByClassName("tabcontent"),i=0;i<o.length;i++)o[i].style.display="none"
+for(a=document.getElementsByClassName("tablink"),i=0;i<a.length;i++)a[i].style.backgroundColor=""
 
-    // Remove the background color of all tablinks/buttons
-    tablinks = document.getElementsByClassName("tablink");
-    for (i = 0; i < tablinks.length; i++) {
-        tablinks[i].style.backgroundColor = "";
-    }
-
-    // Show the specific tab content
-    document.getElementById(cityName).style.display = "block";
-
-    // Add the specific color to the button used to open the tab content
-    elmnt.style.backgroundColor = color;
-}
-
-// Get the element with id="defaultOpen" and click on it
-document.getElementById("defaultOpen").click();
-
+;// Show the specific tab content
+document.getElementById(t).style.display="block",
+// Add the specific color to the button used to open the tab content
+e.style.backgroundColor=n}
 //all the functions below are a hide and show function for the divs under each tab
-
-function androidshow() {
-var docelement = document.getElementById("bigman");
-docelement.style.display='block';
-}
-
-function androidhide() {
-var docelement = document.getElementById("bigman");
-docelement.style.display='none';
-}
-
-function codeshow() {
-var docelement = document.getElementById("codeman");
-docelement.style.display='block';
-}
-
-function codehide() {
-var docelement = document.getElementById("codeman");
-docelement.style.display='none';
-}
-
-function electronicsshow() {
-var docelement = document.getElementById("electronicsman");
-docelement.style.display='block';
-}
-
-function electronicshide() {
-var docelement = document.getElementById("electronicsman");
-docelement.style.display='none';
-}
-
-function othershow() {
-var docelement = document.getElementById("otherman");
-docelement.style.display='block';
-}
-
-function otherhide() {
-var docelement = document.getElementById("otherman");
-docelement.style.display='none';
-}
-
-function introhide() {
-var docelement = document.getElementById("introman");
-docelement.style.display='none';
-}
-function introshow() {
-var docelement = document.getElementById("introman");
-docelement.style.display='block';
-}
+function androidshow(){var t=document.getElementById("bigman")
+t.style.display="block"}function androidhide(){var t=document.getElementById("bigman")
+t.style.display="none"}function codeshow(){var t=document.getElementById("codeman")
+t.style.display="block"}function codehide(){var t=document.getElementById("codeman")
+t.style.display="none"}function electronicsshow(){var t=document.getElementById("electronicsman")
+t.style.display="block"}function electronicshide(){var t=document.getElementById("electronicsman")
+t.style.display="none"}function othershow(){var t=document.getElementById("otherman")
+t.style.display="block"}function otherhide(){var t=document.getElementById("otherman")
+t.style.display="none"}function introhide(){var t=document.getElementById("introman")
+t.style.display="none"}function introshow(){var t=document.getElementById("introman")
+t.style.display="block"}
 //all the functions below are a hide and show function for the firework angle buttons
-function onangleshow() {
-var docelement = document.getElementById("on-angle");
-docelement.style.display='block';
-}
-function onanglehide() {
-var docelement = document.getElementById("on-angle");
-docelement.style.display='none';
-}
-function offangleshow() {
-var docelement = document.getElementById("off-angle");
-docelement.style.display='block';
-}
-function offanglehide() {
-var docelement = document.getElementById("off-angle");
-docelement.style.display='none';
-}
+function onangleshow(){var t=document.getElementById("on-angle")
+t.style.display="block"}function onanglehide(){var t=document.getElementById("on-angle")
+t.style.display="none"}function offangleshow(){var t=document.getElementById("off-angle")
+t.style.display="block"}function offanglehide(){var t=document.getElementById("off-angle")
+t.style.display="none"}
 //all the functions below are a hide and show function for the firework angle text
-function anglexshow() {
-var docelement = document.getElementById("anglex");
-docelement.style.display='block';
-}
-function anglexhide() {
-var docelement = document.getElementById("anglex");
-docelement.style.display='none';
-}
-function angleyshow() {
-var docelement = document.getElementById("angley");
-docelement.style.display='block';
-}
-function angleyhide() {
-var docelement = document.getElementById("angley");
-docelement.style.display='none';
-}
-/*these two here are the ones that are called when the firework button is pressed
+function anglexshow(){var t=document.getElementById("anglex")
+t.style.display="block"}function anglexhide(){var t=document.getElementById("anglex")
+t.style.display="none"}function angleyshow(){var t=document.getElementById("angley")
+t.style.display="block"}function angleyhide(){var t=document.getElementById("angley")
+t.style.display="none"}/*these two here are the ones that are called when the firework button is pressed
 they make the on or off button appear or dissapear and make the canvas show/notshow
 as well as make the angle button visible*/
-function fire() {
-var docelement = document.getElementById("canvas");
-	docelement.style.display='block';
-var docelement = document.getElementById("off-button");
-	docelement.style.display='block';
-var docelement = document.getElementById("on-button");
-	docelement.style.display='none';
-	onangleshow()
-}
+function fire(){var t=document.getElementById("canvas")
+t.style.display="block"
+var t=document.getElementById("off-button")
+t.style.display="block"
+var t=document.getElementById("on-button")
+t.style.display="none",onangleshow()}
 //opposite of above but if pressed either angle button poof and the drit is gone
-function fireoff() {
-var docelement = document.getElementById("canvas");
-	docelement.style.display='none';
-var docelement = document.getElementById("on-button");
-	docelement.style.display='block';
-var docelement = document.getElementById("off-button");
-	docelement.style.display='none';
-	offanglehide()
-	onanglehide()
-}
-
+function fireoff(){var t=document.getElementById("canvas")
+t.style.display="none"
+var t=document.getElementById("on-button")
+t.style.display="block"
+var t=document.getElementById("off-button")
+t.style.display="none",offanglehide(),onanglehide()}
+// Get the element with id="defaultOpen" and click on it
+document.getElementById("defaultOpen").click(),
 //code from here down is courtesy of shreyas : https://github.com/shreyaspandit/html5-fireworks
-
-window.onload = function() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-
-    var W = window.innerWidth;
-    var H = window.innerHeight;
-    canvas.width = W;
-    canvas.height = H;
-
-
-    var fireworks = [];
-
-    //requestAnimationFrame polyfill {{
-
-    (function() {
-        var lastTime = 0;
-        var vendors = ['webkit', 'moz'];
-        for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-            window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-            window.cancelAnimationFrame =
-              window[vendors[x]+'CancelAnimationFrame'] || window[vendors[x]+'CancelRequestAnimationFrame'];
-        }
-
-        if (!window.requestAnimationFrame)
-            window.requestAnimationFrame = function(callback, element) {
-                var currTime = new Date().getTime();
-                var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-                var id = window.setTimeout(function() { callback(currTime + timeToCall); },
-                  timeToCall);
-                lastTime = currTime + timeToCall;
-                return id;
-            };
-
-        if (!window.cancelAnimationFrame)
-            window.cancelAnimationFrame = function(id) {
-                clearTimeout(id);
-            };
-    }());
-
-    //}}
-
-    function resizeCanvas(e) {
-        var nW = window.innerWidth;
-        var nH = window.innerHeight;
-
-        canvas.width = nW;
-        canvas.height = nH;
-    }
-
-    window.addEventListener("resize", resizeCanvas);
-
-    function rad(deg) {
-        return deg * Math.PI / 180;
-    }
-    function pad(str, length, prefix) {
-        while (str.length < length) {
-            str = prefix + str;
-        }
-
-        return str;
-    }
-
-
-    //Fireworks {{
-
-    function Firework() {
-        this.explosions = [];
-        this.x = W / 2;
-        this.y = H;
-
-        this.radius = 4;
-        this.color = "#" + (Math.floor(Math.random() * 0xffffff).toString(16));
-
-        //Random number: Math.floor(Math.random() * (max - min + 1)) + min;
-
-        this.life = Math.floor(Math.random() * ( ((3 * H) / 4) - (H / 2) + 1)) + (H / 2);
-
-        this.velocity = 10;
-        this.angle = Math.floor(Math.random() * (320 - 250 + 1)) + 250;
-		
-
-    }
-
-    Firework.prototype.draw = function() {
-        if(this.life <= 0) {
-
-            var tx = this.x;
-            var ty = this.y;
-
-            fireworks.pop();
-
-            for(var i = 0; i < 100; i++) {
-                new Explosion(tx, ty);
-				var element = document.getElementById("anglex");
-element.innerHTML = (tx);
-var element = document.getElementById("angley");
-element.innerHTML = (ty);
-            }
-
-            fireworks.push(new Firework());
-        }
-
-
-        this.x += this.velocity * Math.cos(rad(this.angle));
-        this.y += this.velocity * Math.sin(rad(this.angle));
-
-        this.life -= 10;
-
-
-        ctx.beginPath();
-        ctx.fillStyle = this.color;
-        ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-        ctx.fill();
-        ctx.closePath();
-    }
-
-    function Explosion(x, y) {
-        this.x = x;
-        this.y = y;
-        this.w = 10;
-        this.h = 10;
-
-        this.hue = Math.round(Math.random() * 360);
-
-        this.alpha = 1;
-        this.fade = 0;
-
-        this.shrink = 0.93;
-        this.radius = 10;
-
-        this.speed = Math.cos(Math.random() * Math.PI / 2) * 15;
-        this.angle = Math.random() * Math.PI * 2;
-
-        this.resistance = 0.95;
-
-
-        this.vx = this.speed * Math.cos(this.angle);
-        this.vy = this.speed * Math.sin(this.angle);
-
-
-        this.gravity = 0.2;
-
-        var that = this;
-        var drawing = setInterval(function() {
-            if (that.radius < 1) {
-                clearInterval(drawing);
-            } else {
-                that.draw();
-            }
-        }, 30);
-
-    };
-
-    Explosion.prototype.draw = function() {
-        this.vx *= this.resistance;
-        this.vy *= this.resistance;
-
-        this.vy += this.gravity;
-
-        this.x += this.vx;
-        this.y += this.vy;
-
-        this.radius *= this.shrink;
-        this.alpha -= this.fade;
-
-
-        ctx.save();
-
-        ctx.globalCompositeOperation = 'lighter';
-
-        var gradient = ctx.createRadialGradient(this.x, this.y, 0.1, this.x, this.y, this.radius);
-        gradient.addColorStop(0.1, "rgba(255,255,255,"+ this.alpha +")");
-        gradient.addColorStop(0.8, "hsla("+ this.hue +", 100%, 50%, "+ this.alpha +")");
-        gradient.addColorStop(1, "hsla("+ this.hue +", 100%, 50%, 0.1)");
-
-        ctx.fillStyle = gradient;
-
-
-        ctx.beginPath();
-        ctx.arc(this.x, this.y, this.radius, Math.PI * 2, false);
-        ctx.closePath();
-        ctx.fill();
-
-        ctx.restore();
-
-
-    };
-
-    //}}
-
-    fireworks.push(new Firework());
-
-    function draw() {
-
-        ctx.fillStyle = "rgba(0, 0, 0, 0.3)";
-        ctx.fillRect(0, 0, W, H);
-
-        for(var i = 0; i < fireworks.length; i++) {
-            fireworks[i].draw()
-        }
-
-        window.requestAnimationFrame(draw);
-    }
-
-    window.requestAnimationFrame(draw);
-
-};
+window.onload=function(){
+//}}
+function t(t){var e=window.innerWidth,n=window.innerHeight
+a.width=e,a.height=n}function e(t){return t*Math.PI/180}
+//Fireworks {{
+function n(){this.explosions=[],this.x=s/2,this.y=d,this.radius=4,this.color="#"+Math.floor(16777215*Math.random()).toString(16),
+//Random number: Math.floor(Math.random() * (max - min + 1)) + min;
+this.life=Math.floor(Math.random()*(3*d/4-d/2+1))+d/2,this.velocity=10,this.angle=Math.floor(71*Math.random())+250}function i(t,e){this.x=t,this.y=e,this.w=10,this.h=10,this.hue=Math.round(360*Math.random()),this.alpha=1,this.fade=0,this.shrink=.93,this.radius=10,this.speed=15*Math.cos(Math.random()*Math.PI/2),this.angle=Math.random()*Math.PI*2,this.resistance=.95,this.vx=this.speed*Math.cos(this.angle),this.vy=this.speed*Math.sin(this.angle),this.gravity=.2
+var n=this,i=setInterval(function(){n.radius<1?clearInterval(i):n.draw()},30)}function o(){l.fillStyle="rgba(0, 0, 0, 0.3)",l.fillRect(0,0,s,d)
+for(var t=0;t<h.length;t++)h[t].draw()
+window.requestAnimationFrame(o)}var a=document.getElementById("canvas"),l=a.getContext("2d"),s=window.innerWidth,d=window.innerHeight
+a.width=s,a.height=d
+var h=[]
+
+;//requestAnimationFrame polyfill {{
+!function(){for(var t=0,e=["webkit","moz"],n=0;n<e.length&&!window.requestAnimationFrame;++n)window.requestAnimationFrame=window[e[n]+"RequestAnimationFrame"],window.cancelAnimationFrame=window[e[n]+"CancelAnimationFrame"]||window[e[n]+"CancelRequestAnimationFrame"]
+window.requestAnimationFrame||(window.requestAnimationFrame=function(e,n){var i=(new Date).getTime(),o=Math.max(0,16-(i-t)),a=window.setTimeout(function(){e(i+o)},o)
+return t=i+o,a}),window.cancelAnimationFrame||(window.cancelAnimationFrame=function(t){clearTimeout(t)})}(),window.addEventListener("resize",t),n.prototype.draw=function(){if(this.life<=0){var t=this.x,o=this.y
+h.pop()
+for(var a=0;100>a;a++){new i(t,o)
+var s=document.getElementById("anglex")
+s.innerHTML=t
+var s=document.getElementById("angley")
+s.innerHTML=o}h.push(new n)}this.x+=this.velocity*Math.cos(e(this.angle)),this.y+=this.velocity*Math.sin(e(this.angle)),this.life-=10,l.beginPath(),l.fillStyle=this.color,l.arc(this.x,this.y,this.radius,2*Math.PI,!1),l.fill(),l.closePath()},i.prototype.draw=function(){this.vx*=this.resistance,this.vy*=this.resistance,this.vy+=this.gravity,this.x+=this.vx,this.y+=this.vy,this.radius*=this.shrink,this.alpha-=this.fade,l.save(),l.globalCompositeOperation="lighter"
+var t=l.createRadialGradient(this.x,this.y,.1,this.x,this.y,this.radius)
+t.addColorStop(.1,"rgba(255,255,255,"+this.alpha+")"),t.addColorStop(.8,"hsla("+this.hue+", 100%, 50%, "+this.alpha+")"),t.addColorStop(1,"hsla("+this.hue+", 100%, 50%, 0.1)"),l.fillStyle=t,l.beginPath(),l.arc(this.x,this.y,this.radius,2*Math.PI,!1),l.closePath(),l.fill(),l.restore()},
+//}}
+h.push(new n),window.requestAnimationFrame(o)}
